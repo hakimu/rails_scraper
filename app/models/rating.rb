f0bb61fadae2a978003dd07a95dc14d64ae7daec
Hakimu Jackson <hakimu@newrelic.com>
2019-06-13 07:50:39 -0700

    Removed the before_create callback for Rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,11 +3,4 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   validates :star, presence: true
   validates :star, inclusion: { in: [1,2,3,4,5], message: 'Star should be 1-5.' }
-
-  before_create :check_admin_before_create
-  
-  def check_admin_before_create
-    admin_key = ENV["MY_API_KEY"]
-    raise CannotCreateRatingError.new('Not authorized') unless admin_key 
-  end
 end
