a9a99df67d18ab21eac661539d77655c2b82e3c6
Hakimu Jackson <hakimu@newrelic.com>
2020-03-14 16:51:39 -0700

    Update song controller spec for auth

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe SongsController, type: :controller do
   describe "GET index" do
-    context 'when there is no longged in user' do
-      it "#index page" do
+    context 'when there is not a logged in user' do
+      it "redirects to the home page for login" do
         get :index
         expect(response).to have_http_status(:redirect)
+      end
+    end
+    context 'when there is a loggin in user' do
+      it 'returns songs' do
+        user = create(:user)
+        session[:user_id] = user.id
+        get :index
+        expect(response).to have_http_status(:success)
       end
     end
   end
